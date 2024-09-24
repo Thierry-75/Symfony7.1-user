c159cb01abf3f189ab2b5b9ae4bda97411c34ff7
Enki <104570540+Thierry-75@users.noreply.github.com>
2024-09-24 12:52:20 +0200

    implement token emil

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationFormType;
+use App\Repository\UserRepository;
 use App\Security\AppUserAuthenticator;
+use App\Service\JWTService;
 use App\Service\SendMailService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -22,7 +24,8 @@ class RegistrationController extends AbstractController
         UserPasswordHasherInterface $userPasswordHasher,
         Security $security,
         EntityManagerInterface $entityManager,
-        SendMailService $mail
+        SendMailService $mail,
+        JWTService $jwt
     ): Response {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -39,6 +42,18 @@ class RegistrationController extends AbstractController
 
             $entityManager->persist($user);
             $entityManager->flush();
+
+            //generate jwt
+            $header = [
+                'typ' => 'JWT',
+                'alg' => 'HS256'
+            ];
+            $payload = [
+                'user_id' => $user->getId()
+            ];
+            //token
+            $token = $jwt->generate($header, $payload, $this->getParameter('app.jwtsecret'));
+
             //envoi du mail
             $mail->envoi(
                 'no-reply@monblog.org',
@@ -46,7 +61,8 @@ class RegistrationController extends AbstractController
                 'Activation de votrecompte sur notre site',
                 'register',
                 [
-                    'user' => $user
+                    'user' => $user,
+                    'token' => $token
                 ]
             );
 
@@ -56,5 +72,62 @@ class RegistrationController extends AbstractController
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form,
         ]);
+    }
+
+    #[Route('/verif/{token}', name: 'verify_user')]
+    public function verifyUser($token, JWTService $jwt, UserRepository $userRepository, EntityManagerInterface $em): Response
+    {
+        // isValid && isExpired && check
+        if ($jwt->isValid($token) && !$jwt->isExpired($token) && $jwt->check($token, $this->getParameter('app.jwtsecret'))) {
+            $payload = $jwt->getPayload($token);
+            $user  = $userRepository->find($payload['user_id']);
+            if ($user && !$user->getIsVerified()) {
+                $user->setIsVerified(true);
+                $em->persist($user);
+                $em->flush();
+                $this->addFlash('alert-success', 'Votre compte a été activé !');
+                return $this->redirectToRoute('app_main');  // changer la route  ? profile
+            }
+        }
+        $this->addFlash('alert-danger', 'Le token est invalide !');
+        return $this->redirectToRoute('app_login');
+    }
+
+    #[Route('/renvoiverif', name: 'resend_verif')]
+    public function resendVerif(UserRepository $userRepository, JWTService $jwt, SendMailService $mail): Response
+    {
+        $user = $this->getUser();
+        if (!$user) {
+            $this->addFlash('alert-danger', 'Vous devez être connecté pour accéder à cette page !');
+            return $this->redirectToRoute('app_login');
+        }
+        if ($user->getIsVerified()) {
+            $this->addFlash('alert-warning', 'Ce compte est déjà activé !');
+            return $this->redirectToRoute('app_main');  // redirect to profil
+        }
+        //generate jwt
+        $header = [
+            'typ' => 'JWT',
+            'alg' => 'HS256'
+        ];
+        $payload = [
+            'user_id' => $user->getId()
+        ];
+        //token
+        $token = $jwt->generate($header, $payload, $this->getParameter('app.jwtsecret'));
+
+        //envoi du mail
+        $mail->envoi(
+            'no-reply@monblog.org',
+            $user->getEmail(),
+            'Activation de votrecompte sur notre site',
+            'register',
+            [
+                'user' => $user,
+                'token' => $token
+            ]
+        );
+        $this->addFlash('alert-success', 'Email de vérification envoyé !');
+        return $this->redirectToRoute('app_main');
     }
 }
